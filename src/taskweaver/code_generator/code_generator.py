ab85d94e3529d590e267766c50d8ad7b15f82606
Tonic <josephrichardpollack@gmail.com>
2023-12-19 20:00:49 +0100

    refactor taskweaver (#31)
    Co-authored-by: tonic <josephp@tuta.io>

--- src/taskweaver/code_generator/code_generator.py
+++ src/taskweaver/code_generator/code_generator.py
@@ -1,13 +1,36 @@
-# code_generator/code_generator.py
+from taskweaver.code_executor import code_executor
+from taskweaver.config import Config
+from taskweaver.logging import Logger
+from taskweaver.memory import Memory
 
 class CodeGenerator:
-    def __init__(self, taskweaver):
-        self.taskweaver = taskweaver
+    def __init__(self, interpreter: CodeInterpreter, config: Config, logger: Logger):
+        self.interpreter = interpreter
+        self.config = config
+        self.logger = logger
 
-    def generate_code(self, plan):
-        code_snippets = []
-        for step in plan:
-            # Assume taskweaver has a method to generate code for a given step
-            generated_code = self.taskweaver.generate_code_for_step(step)
-            code_snippets.append(generated_code)
-        return code_snippets
+    def verify_code(self, code_snippet: str) -> bool:
+        """
+        Verify the generated code snippet.
+        Returns True if the code is verified, otherwise False.
+        """
+        allowed_modules = self.config.allowed_modules
+        errors = self.interpreter.verify(code_snippet, allowed_modules)
+        if errors:
+            self.logger.error(f"Code verification error: {errors}")
+            return False
+        return True
+
+    def generate_code(self, plan: dict, memory: Memory) -> str:
+        """
+        Generate code based on a plan. The code is then verified and executed.
+        """
+        code_snippet = self.interpreter.generate(plan, memory)
+        
+        if self.verify_code(code_snippet):
+            # Execute the code if verification passes
+            execution_result = self.interpreter.execute(code_snippet)
+            return execution_result
+        else:
+            self.logger.error("Code verification failed.")
+            return "Code verification failed."
--- src/taskweaver/plugins/deploy_sql_database.py
+++ src/taskweaver/plugins/deploy_sql_database.py
@@ -32,15 +32,33 @@ class SQLiteDatabaseDeployment:
         for table_name, schema in entities_definition.items():
             self.create_table(table_name, schema)
     
-    # Here you can include methods to seed initial data if necessary
-    # ...
+    # include methods to seed initial data if necessary
 
-# Example usage
 if __name__ == "__main__":
     db_deployment = SQLiteDatabaseDeployment()
     tables_schema = {
-        "users": "id INTEGER PRIMARY KEY, name TEXT, email TEXT",
-        "orders": "id INTEGER PRIMARY KEY, user_id INTEGER, date TEXT",
-        # Add more tables and schemas as needed
+    "ClientBackground": "id INTEGER PRIMARY KEY, company_history TEXT, mission TEXT, vision TEXT, strategic_objectives TEXT",
+    "IndustryData": "id INTEGER PRIMARY KEY, market_size TEXT, trends TEXT, competitors TEXT, regulatory_environment TEXT",
+    "StakeholderInfo": "id INTEGER PRIMARY KEY, name TEXT, role TEXT, organization TEXT, contact_info TEXT",
+    "OperationalData": "id INTEGER PRIMARY KEY, sales_data TEXT, production_data TEXT, supply_chain_data TEXT, employee_info TEXT",
+    "FinancialData": "id INTEGER PRIMARY KEY, financial_statements TEXT, budgets TEXT, financial_analysis TEXT",
+    "CustomerDatabase": "id INTEGER PRIMARY KEY, demographics TEXT, satisfaction_level TEXT, purchase_history TEXT",
+    "DocumentManagementSystem": "id INTEGER PRIMARY KEY, document_name TEXT, category TEXT, content TEXT, storage_location TEXT",
+    "IndustryBenchmarking": "id INTEGER PRIMARY KEY, benchmark_metrics TEXT, industry_standards TEXT, best_practices TEXT",
+    "CaseStudyData": "id INTEGER PRIMARY KEY, study_name TEXT, study_details TEXT, insights TEXT",
+    "QualitativeDataStorage": "id INTEGER PRIMARY KEY, interview_data TEXT, focus_group_data TEXT, coded_data TEXT",
+    "SQLQueryOptimization": "id INTEGER PRIMARY KEY, query TEXT, optimization_techniques TEXT, performance_metrics TEXT",
+    "SQLQueryTesting": "id INTEGER PRIMARY KEY, query TEXT, test_results TEXT, reliability_metrics TEXT",
+    "SQLQueryDocumentation": "id INTEGER PRIMARY KEY, query TEXT, documentation TEXT, use_cases TEXT",
+    "AnnualFinancialReport": "id INTEGER PRIMARY KEY, year TEXT, total_revenue TEXT, net_income TEXT, assets TEXT, liabilities TEXT, equity TEXT, cash_flow_statement TEXT",
+    "CorporateGovernance": "id INTEGER PRIMARY KEY, board_members TEXT, governance_policies TEXT, ethical_guidelines TEXT, compliance_status TEXT",
+    "MarketPerformance": "id INTEGER PRIMARY KEY, stock_price TEXT, market_capitalization TEXT, dividend_info TEXT, analyst_ratings TEXT",
+    "SustainabilityReport": "id INTEGER PRIMARY KEY, environmental_impact TEXT, social_responsibility TEXT, governance_practices TEXT, sustainability_goals TEXT",
+    "ResearchAndDevelopment": "id INTEGER PRIMARY KEY, r_and_d_investments TEXT, innovation_initiatives TEXT, patent_info TEXT, research_outcomes TEXT",
+    "EmployeeInformation": "id INTEGER PRIMARY KEY, total_employees TEXT, employee_turnover TEXT, training_and_development TEXT, diversity_statistics TEXT",
+    "CustomerSatisfactionReport": "id INTEGER PRIMARY KEY, survey_results TEXT, customer_feedback TEXT, improvement_measures TEXT, loyalty_metrics TEXT",
+    "OperationalAchievements": "id INTEGER PRIMARY KEY, milestones_achieved TEXT, operational_efficiency TEXT, cost_savings TEXT, new_markets_entered TEXT",
+    "RiskManagement": "id INTEGER PRIMARY KEY, risk_assessment TEXT, mitigation_strategies TEXT, compliance_issues TEXT, risk_monitoring TEXT"
     }
     db_deployment.deploy(tables_schema)
+
