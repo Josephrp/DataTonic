82d87b0c8cac0efa7a430974ae6e65b2cb2804d1
tonic <josephp@tuta.io>
2023-12-21 00:09:35 +0100

    add interface for DataTonic :-)
    what's wrong with chainlit ?

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
 import json
-import asyncio
 import os
+import asyncio
+from chainlit import chainlit as cl
 from src.semantic_kernel.semantic_kernel_module import SemanticKernelDataModule
-from src.taskweaver.taskweaver_module import TaskWeaverDataProcessor
+from src.tonicweaver.taskweaver_module import TaskWeaverDataProcessor
 from src.autogen.autogen_module import AutoGenModule
 
-async def main():
-    with open('OAI_CONFIG_LIST.json', 'r') as file:
-        OAI_CONFIG_LIST = json.load(file)
+async def process_user_input(user_input):
     semantic_kernel = SemanticKernelDataModule()
     taskweaver = TaskWeaverDataProcessor()
     autogen_module = AutoGenModule(memgpt_memory_path="./src/autogen/MemGPT", openai_api_key=os.getenv('OPENAI_API_KEY'))
+    
     autogen_module.semantic_kernel = semantic_kernel
     autogen_module.taskweaver = taskweaver
-
-    user_input = input("Please provide a description for the task you'd like to perform, identify your objectives, a plan on how to achieve it according to you, and a list of the final results you expect: ")
-
+    
     sow_document = await semantic_kernel.create_and_fetch_sow(user_input)
+    
+    executed_plan = await autogen_module.AutoGenModule(sow_document)
+    
+    return executed_plan
 
-    executed_plan = autogen_module.AutoGenModule(sow_document)
+@cl.on_chat_start
+async def start():
+    # Initial setup if needed
+    pass
 
-    print(f"Executed Plan: \n{executed_plan}")
+@cl.on_message
+async def main(message: cl.Message):
+    user_input = message.content
+    executed_plan = await process_user_input(user_input)
+    
+    # Assuming the executed_plan is a string or something that can be converted to a string
+    await cl.Message(content=str(executed_plan)).send()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    cl.run(main)
